d2ac1edc4c6b140d1900bec2c299c2e74cf0c8f3
Blaire Xinyu Pang <xinyup@umich.edu>
2023-12-19 10:55:52 -0500

    5 test folder

---
+++ week5 Unit Tests/test/test_calculator.py
@@ -0,0 +1,35 @@
+"""
+pytest
+
+unit testing 
+
+"""
+
+from calculator import square
+import pytest
+
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+def test_zero():
+    assert square(0) == 0
+
+
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat") 
+
+
+
+
+
+
+
+
