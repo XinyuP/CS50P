f32df6a106ab755afe20ebab0bed4c2d991a8826
Blaire Xinyu Pang <xinyup@umich.edu>
2023-12-19 10:54:18 -0500

    5 pytest

---
+++ week5 Unit Tests/test_calculator2.py
@@ -0,0 +1,30 @@
+"""
+pytest
+
+unit testing 
+
+"""
+
+from calculator import square
+import pytest
+
+
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
+
+
+
+
+
+
+
+
+
+
+
+
