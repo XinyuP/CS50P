37c3506c97e980019c112cb274f8f1c05c753e10
Blaire Xinyu Pang <xinyup@umich.edu>
2023-12-21 05:05:03 -0500

    p0

---
+++ problemSet0/tip/tip.py
@@ -0,0 +1,30 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    # d = d[1:]
+    # d = d.removeprefix("$")
+    # d = d.replace("$", "")
+    d = d.lstrip("$")
+    return float(d)
+    
+
+
+
+def percent_to_float(p):
+    # p = p[:-1]
+    # p = p.removesuffix("%")
+    # p = p.replace("%", "")
+    p = p.rstrip("%")
+
+
+    return float(p) / 100
+
+
+
+
+main()
