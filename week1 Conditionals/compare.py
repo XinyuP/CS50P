1662afcb999799358868047b8abcaebe92b79863
Blaire Xinyu Pang <xinyup@umich.edu>
2023-12-21 05:55:56 -0500

    lec1 conditional

---
+++ week1 Conditionals/compare.py
@@ -0,0 +1,25 @@
+x = int(input("What is x? "))
+y = int(input("What is y? "))
+
+#### if elif else
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+# elif x == y:
+else:
+    print("x is equal to y")
+
+
+#### or 
+if x < y or x > y: 
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
+#### != 
+if x != y: 
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
