1591debb97a92e2bcce9d9f6e7aa417da393ae7f
Blaire Xinyu Pang <xinyup@umich.edu>
2023-12-22 14:38:12 -0500

    lec4 library statistics

---
+++ week4 Libraries/average1.py
@@ -0,0 +1,34 @@
+"""
+statistics
+
+statistics.mean(data)
+
+data: sequence or iterable
+
+"""
+
+import statistics
+
+print(statistics.mean([100, 90]))
+
+print(statistics.median([100, 90, 90, 80, 70, 60]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
