7a11fdb11dcb545b3470d17a6e8b05e31ae8acb8
Blaire Xinyu Pang <xinyup@umich.edu>
2023-12-22 14:39:03 -0500

    lec4 library package, PyPI, cowsay, pip

---
+++ week4 Libraries/say.py
@@ -0,0 +1,110 @@
+"""
+package
+
+third-party libraries
+
+package is a module implemented in a folder
+(not just a file, but a folder)
+
+
+
+a package is a third-party library we can install on our computer 
+
+
+
+
+
+PyPI is a repo or directory of all available third-party packages currently available
+
+Python Package Index
+
+
+https://pypi.org/
+
+
+
+
+
+cowsay
+
+
+
+
+pip is a program that comes with Python itself 
+that allows you to install packages onto your computer
+
+
+
+
+
+"""
+import cowsay
+import sys
+
+
+if len(sys.argv) == 2:
+    # cowsay.cow("hello, " + sys.argv[1])
+    cowsay.tux("hello, " + sys.argv[1])
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# cowsay.cow('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit rhoncus nibh. Mauris mi mauris, molestie vel metus sit amet, aliquam vulputate nibh.')
+
+
+
+# my_fish = r'''
+# \
+#  \  
+#         /`·.¸
+#      /¸...¸`:·
+#  ¸.·´  ¸   `·.¸.·´)
+# : © ):´;      ¸  {
+#  `·.¸ `·  ¸.·´\`·¸)
+#      `\\´´\¸.·´
+# '''
+
+
+# cowsay.draw('Sharks are my best friend', my_fish)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
