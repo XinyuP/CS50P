924fe1bb926abc2ead2d829b8a0df11ad54b0a06
Blaire Xinyu Pang <xinyup@umich.edu>
2023-12-22 20:16:05 -0500

    p4 pyfiglet

--- problemSet4/figlet/figlet.py
+++ problemSet4/figlet/figlet.py
@@ -76,23 +76,63 @@
 
 
 
+
+# #############################################
+
+# import sys
+# from pyfiglet import Figlet
+# import random
+
+# def main():
+#     get_figlet_text()
+
+# def get_figlet_text():
+#     figlet = Figlet()
+#     font_list = figlet.getFonts()
+
+#     if len(sys.argv) == 1:
+#         f = random.choice(font_list)
+#         figlet.setFont(font=f) 
+        
+#     elif len(sys.argv) == 3:
+#         if sys.argv[1] not in ["-f", "--font"]:
+#             sys.exit("Invalid usage")
+#         f = sys.argv[2]
+#         if f not in font_list:
+#             sys.exit("Invalid usage")
+#         figlet.setFont(font=f)
+
+#     else:
+#         sys.exit("Invalid usage")
+    
+#     text = input("What's the text? ")
+#     print(figlet.renderText(text))
+
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+
 ##############################################
 
 import sys
 from pyfiglet import Figlet
 import random
 
+figlet = Figlet()
+font_list = figlet.getFonts()
+
+
 def main():
-    get_figlet_text()
-
-def get_figlet_text():
-    figlet = Figlet()
-    font_list = figlet.getFonts()
-
     if len(sys.argv) == 1:
         f = random.choice(font_list)
         figlet.setFont(font=f) 
-        
+        get_figlet_text("What's the text? ", f)
+
     elif len(sys.argv) == 3:
         if sys.argv[1] not in ["-f", "--font"]:
             sys.exit("Invalid usage")
@@ -101,12 +141,16 @@ def get_figlet_text():
         if f not in font_list:
             sys.exit("Invalid usage")
 
-        figlet.setFont(font=f)
-        
+        get_figlet_text("What's the text? ", f)
+
     else:
         sys.exit("Invalid usage")
+    
 
-    text = input("What's the text? ")
+
+def get_figlet_text(prompt, f):
+    text = input(prompt)
+    figlet.setFont(font=f)
     print(figlet.renderText(text))
 
 
