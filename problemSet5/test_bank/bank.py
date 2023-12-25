a4714cc72c139cae8db9a975ad97abdd912632c0
Blaire Xinyu Pang <xinyup@umich.edu>
2023-12-25 17:01:04 -0500

    modify p5 bank

--- problemSet5/test_bank/bank.py
+++ problemSet5/test_bank/bank.py
@@ -1,15 +1,33 @@
 def main():
-    greeting = (input("Greeting: "))
+    greeting = (input("Greeting: ")).strip().lower()
     print(f"${value(greeting)}")
 
 def value(greeting):
-    if greeting.strip().lower().startswith("h"):
-        if greeting.strip().lower() == ("hello"):
-            return 0
-        else:
-            return 20
+    if greeting.startswith("hello"):
+        return 0
+    elif greeting.startswith("h"):
+        return 20
     else:
         return 100
     
 if __name__ == "main":
     main()
+
+
+
+
+# def main():
+#     greeting = (input("Greeting: "))
+#     print(f"${value(greeting)}")
+
+# def value(greeting):
+#     if greeting.strip().lower().startswith("h"):
+#         if greeting.strip().lower() == ("hello"):
+#             return 0
+#         else:
+#             return 20
+#     else:
+#         return 100
+    
+# if __name__ == "main":
+#     main()
