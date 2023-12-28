9053f459de2fe7ad345d97d4d91f4dcf59ba5516
Blaire Xinyu Pang <xinyup@umich.edu>
2023-12-28 15:03:15 -0500

    [] set of characters, [^] complementing the set, re.search(r"^[^@]+@[^@]+\.edu$", email)

--- Week7 Regular Expressions/validate_re.py
+++ Week7 Regular Expressions/validate_re.py
@@ -2,11 +2,12 @@ import re
 
 email = input("What's your email? ").strip()
 
-# if re.search(".+@.+", email):
-if re.search(r"..*@..*\.edu", email):
-    print("Valid")
+if re.search(r"^[^@]+@[^@]+\.edu$", email):
+# if re.search(r"^..*@..*\.edu$", email):
+
+    print(email, "Valid")
 else:
-    print("Invali d")
+    print(email, "Invalid")
 
 
 """
@@ -20,12 +21,61 @@ else:
 
 
 raw strings are strings that don't format special characters
-each character is taken at face-value`
+each character is taken at face-value
+
+-> Placing an r in front of the strings
+tells the python interpreter to treat the string as a raw string,
+similar to how placing an f in front of a string tells the python interpreter to
+treat as a format string
+
+
+eg.
+\n in a reguar string becomes a special newline character
+\n in a raw string is just a single "\" and a single "n"
+
+
 
 
 .+ 
 is equivalent to 
 ..*
+
+
+
+
+
+^ matches the "start" of the string
+
+$ matches the "end" of the string 
+just befire the newline at the end of the string
+
+
+
+
+
+
+
+re.search(r"^.+[@].+\.edu$", email)
+
+
+
+
+[] set of characters
+[^] complementing the set
+
+if re.search(r"^[^@]+@[^@]+\.edu$", email)
+
+^: match from the start 
+[^@]: "any char except @"
+[^@]+: one or more "any char except @"
+
+
+\.edu: followed by literally .edu
+
+
+$: match at the end 
+
+ 
 
 
 
