977f5e61d54f38c99564e21f9ec683d725358c28
Blaire Xinyu Pang <xinyup@umich.edu>
2023-12-29 10:21:35 -0500

    (\w+\.)? 0 or 1 subdomain

--- Week7 Regular Expressions/validate_re_case.py
+++ Week7 Regular Expressions/validate_re_case.py
@@ -2,15 +2,12 @@ import re
 
 email = input("What's your email? ").strip()
 
-# if re.search(r"^..*@..*\.edu$", email):
-# if re.search(r"^[^@]+@[^@]+\.edu$", email):
-# if re.search(r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.edu$", email):
-# if re.search(r"^\w+@\w+\.edu$", email):
-# if re.search(r"^(\w|\s)+@\w+\.(edu|com|gov|net|org)$", email): # if we allow whitespace
-# if re.search(r"^(\w|.|-)+@\w+\.(edu|com|gov|net|org)$", email):
+
 # if re.search(r"^\w+@\w+\.(edu|com|gov|net|org)$", email, re.IGNORECASE):
-if re.search(r"^\w+@\w+\.edu$", email, re.IGNORECASE):
-    print(email, "Valid")
+if re.search(r"^\w+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
+# if re.search(r"^(\w|\.)+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
+# if re.search(r"^\w+@(\w+\.)*\w+\.edu$", email, re.IGNORECASE):
+    print("Valid")
 else:
     print(email, "Invalid")
 
@@ -37,6 +34,10 @@ Return None if no position in the string matches the pattern;
 note that this is different from finding a zero-length match at some point in the string.
 
 
+
+
+(\w+\.)? 0 or 1 subdomain
+(\w+\.)* 0 or more subdomain
 
 
 
