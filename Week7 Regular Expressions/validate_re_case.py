4e159b55bbc8379933aee10db26bfbe5ca7f4bec
Blaire Xinyu Pang <xinyup@umich.edu>
2023-12-29 10:24:40 -0500

    [a-zA-Z0-9_\.]+ is equivalent to (\w\.)+

--- Week7 Regular Expressions/validate_re_case.py
+++ Week7 Regular Expressions/validate_re_case.py
@@ -6,7 +6,8 @@ email = input("What's your email? ").strip()
 # if re.search(r"^\w+@\w+\.(edu|com|gov|net|org)$", email, re.IGNORECASE):
 # if re.search(r"^\w+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
 # if re.search(r"^(\w|\.)+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
-if re.search(r"^\w+@(\w+\.)*\w+\.edu$", email, re.IGNORECASE):
+# if re.search(r"^\w+@(\w+\.)*\w+\.edu$", email, re.IGNORECASE):
+if re.search(r"^[a-zA-Z0-9_\.]+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
     print("Valid")
 else:
     print(email, "Invalid")
