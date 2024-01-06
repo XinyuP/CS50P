efd6c6a6dab3bfb698e79b8f2d82952e038478d6
Blaire Xinyu Pang <xinyup@umich.edu>
2024-01-05 19:40:01 -0500

    @classmethod cls  Student.get()

---
+++ week8 Object-Oriented Programming/studen_class_property copy.py
@@ -0,0 +1,28 @@
+"""
+
+"""
+def main():
+    student = Student.get()
+    print(student)
+
+
+class Student:
+    def __init__(self, name, house): 
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    @classmethod # be able to call get without having a student object in the universe already 
+    def get(cls): # cls is a reference to the class itself 
+        name = input("Name: ")
+        house = input("House: ") 
+        return cls(name, house) # cls automatically pass in reference to the class itself
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
