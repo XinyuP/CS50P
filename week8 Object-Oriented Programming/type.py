879b2a2b234f807dff3578ba077b1e587a6a3bd0
Blaire Xinyu Pang <xinyup@umich.edu>
2024-01-05 19:40:49 -0500

    type of different mathod

---
+++ week8 Object-Oriented Programming/type.py
@@ -0,0 +1,12 @@
+print(type(50)) # <class 'int'>
+print(type(int())) # <class 'int'>
+
+print(type("hello, world")) # <class 'str'>
+print(type(str())) # <class 'str'>
+
+print(type([])) # <class 'list'>
+print(type(list)) # <class 'type'>
+print(type(list())) # list() = [] # <class 'list'>
+
+print(type({})) # <class 'dict'>
+print(type(dict())) # <class 'dict'>
