5761a3702fbb3a7734e9de6a25e6a256a05e0c31
Blaire Xinyu Pang <xinyup@umich.edu>
2024-01-09 02:39:49 -0500

    match case _

--- week1 Conditionals/house.py
+++ week1 Conditionals/house.py
@@ -2,17 +2,17 @@ name = input("What's your name? ")
 
 #### match ####
 
-# match name:
-#     case "Harry":
-#         print("Gryffindor")
-#     case "Hermione":
-#         print("Gryffindor")
-#     case "Ron":
-#         print("Gryffindor")
-#     case "Draco":
-#         print("Slytherin")
-#     case _:
-#         print("Who?")
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
 
 
 match name:
